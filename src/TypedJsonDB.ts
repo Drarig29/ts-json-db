6fd7b641d34d3801e76af3f02acc257e64fea62a
Drarig29 <corentingirard.dev@gmail.com>
2020-03-27 02:08:46 +0100

    All working

--- src/TypedJsonDB.ts
+++ src/TypedJsonDB.ts
@@ -1,7 +1,10 @@
-import { JsonDB } from "node-json-db";
+import { JsonDB, FindCallback } from "node-json-db";
+import { DataError } from "node-json-db/dist/lib/Errors";
 
 type EntryType = "singles" | "arrays" | "dictionaries";
 type Dictionary<V> = { [key: string]: V };
+
+type AllUnionKeys<T> = T extends any ? keyof T : never;
 
 export type ContentBase = {
     [path in EntryType]: any;
@@ -32,15 +35,105 @@ export default class TypedJsonDB<ContentDef extends ContentBase> {
         this.internalDB = new JsonDB(filename, saveOnPush, humanReadable, separator);
     }
 
-    set<Path extends keyof ContentDef["singles"]>(path: Path, data: ContentDef["singles"][Path]) {
-        this.internalDB.push(path.toString(), data);
+    /**
+     * Push initial data when it doesn't exist.
+     * @param path 
+     * @param initialValue 
+     */
+    pushIfNotExists(path: string, initialValue: any): void {
+        if (!this.internalDB.exists(path)) {
+            this.internalDB.push(path, initialValue);
+        }
     }
 
-    setArray<Path extends keyof ContentDef["arrays"]>(path: Path, data: ContentDef["arrays"][Path][]) {
-        this.internalDB.push(path.toString(), data);
+    /**
+     * Get data from database.
+     * @param path 
+     * @returns null if not found.
+     */
+    get(path: string): any {
+        if (this.internalDB.exists(path)) {
+            return this.internalDB.getData(path);
+        }
+
+        return null;
     }
 
-    setDictionary<Path extends keyof ContentDef["dictionaries"]>(path: Path, data: Dictionary<ContentDef["dictionaries"][Path]>) {
-        this.internalDB.push(path.toString(), data);
+    /**
+     * Check for existing datapath
+     * @param dataPath
+     */
+    exists<Path extends AllUnionKeys<ContentDef["singles"] | ContentDef["arrays"] | ContentDef["dictionaries"]>>(path: Path): boolean {
+        return this.internalDB.exists(path.toString());
+    }
+
+    /**
+     * Find all specific entry in an array / object.
+     * @param rootPath Base dataPath from where to start searching
+     * @param callback Method to filter the result and find the wanted entry. Receive the entry and its index.
+     */
+    filter<T, Path extends AllUnionKeys<ContentDef["singles"] | ContentDef["arrays"] | ContentDef["dictionaries"]>>(rootPath: Path, callback: FindCallback): T[] | undefined {
+        return this.internalDB.filter<T>(rootPath.toString(), callback);
+    }
+
+    /**
+     * Find a specific entry in an array / object.
+     * @param rootPath Base dataPath from where to start searching
+     * @param callback Method to filter the result and find the wanted entry. Receive the entry and its index.
+     */
+    find<T, Path extends AllUnionKeys<ContentDef["singles"] | ContentDef["arrays"] | ContentDef["dictionaries"]>>(rootPath: Path, callback: FindCallback): T | undefined {
+        return this.internalDB.find<T>(rootPath.toString(), callback);
+    }
+
+    delete<Path extends AllUnionKeys<ContentDef["singles"] | ContentDef["arrays"] | ContentDef["dictionaries"]>>(path: Path) {
+        this.internalDB.delete(path.toString());
+    }
+
+    set<Path extends keyof ContentDef["singles"]>(path: Path, data: ContentDef["singles"][Path], override?: boolean) {
+        this.internalDB.push(path.toString(), data, override);
+    }
+
+    arraySet<Path extends keyof ContentDef["arrays"]>(path: Path, data: ContentDef["arrays"][Path][], override?: boolean) {
+        this.internalDB.push(path.toString(), data, override);
+    }
+
+    arrayPush<Path extends keyof ContentDef["arrays"]>(path: Path, data: ContentDef["arrays"][Path], override?: boolean) {
+        this.internalDB.push(`${path.toString()}[]`, data, override);
+    }
+
+    arrayPushAt<Path extends keyof ContentDef["arrays"]>(path: Path, index: number, data: ContentDef["arrays"][Path], override?: boolean) {
+        this.internalDB.push(`${path.toString()}[${index}]`, data, override);
+    }
+
+    arrayGet<Path extends keyof ContentDef["arrays"]>(path: Path): ContentDef["arrays"][Path] | null {
+        return this.get(path.toString());
+    }
+
+    arrayGetAt<Path extends keyof ContentDef["arrays"]>(path: Path, index: number): ContentDef["arrays"][Path] | null {
+        return this.get(`${path.toString()}[${index}]`);
+    }
+
+    dictionarySet<Path extends keyof ContentDef["dictionaries"]>(path: Path, data: Dictionary<ContentDef["dictionaries"][Path]>, override?: boolean) {
+        this.internalDB.push(path.toString(), data, override);
+    }
+
+    dictionaryPush<Path extends keyof ContentDef["dictionaries"]>(path: Path, property: string, data: ContentDef["dictionaries"][Path], override?: boolean) {
+        if (!property.match(/^[^\/[]+\/?$/)) {
+            throw new DataError("You can't set a complex property. The return type wouldn't be correct. Use the internal database.", 99);
+        }
+
+        this.internalDB.push(`${path.toString()}/${property}`, data, override);
+    }
+
+    dictionaryGet<Path extends keyof ContentDef["dictionaries"]>(path: Path): ContentDef["dictionaries"][Path] | null {
+        return this.get(path.toString());
+    }
+
+    dictionaryGetAt<Path extends keyof ContentDef["dictionaries"]>(path: Path, property: string): ContentDef["dictionaries"][Path] | null {
+        if (!property.match(/^[^\/[]+\/?$/)) {
+            throw new DataError("You can't set a complex property. The return type wouldn't be correct. Use the internal database.", 99);
+        }
+
+        return this.get(`${path.toString()}/${property}`);
     }
 }
--- test/TypedJsonDB.spec.ts
+++ test/TypedJsonDB.spec.ts
@@ -19,14 +19,46 @@ describe("TypedJsonDB tests", () => {
     it("should set an array", () => {
         let db = new TypedJsonDB<DbContent>("config.json");
         let restaurant: Restaurant = { chef: "foo", name: "bar", memberCount: 10, turnOver: 10000 }
-        db.setArray("/restaurants", [restaurant]);
+        db.arraySet("/restaurants", [restaurant]);
         assert(db.internalDB.getData("/restaurants")[0] === restaurant);
+    });
+
+    it("should push to an array", () => {
+        let db = new TypedJsonDB<DbContent>("config.json");
+        let restaurant: Restaurant = { chef: "eheh", name: "hello", memberCount: 8, turnOver: 2000 }
+        db.arrayPush("/restaurants", restaurant);
+        assert(db.internalDB.getData("/restaurants[-1]") === restaurant);
+    });
+
+    it("should push and retrieve in an array at a given position", () => {
+        let db = new TypedJsonDB<DbContent>("config.json");
+        let restaurant: Restaurant = { chef: "eheh", name: "hello", memberCount: 8, turnOver: 2000 }
+        db.arrayPushAt("/restaurants", 2, restaurant);
+        assert(db.arrayGetAt("/restaurants", 2) === restaurant);
     });
 
     it("should set a dictionary", () => {
         let db = new TypedJsonDB<DbContent>("config.json");
         let data = { test1: "foo", test2: "bar" };
-        db.setDictionary("/teams", data);
+        db.dictionarySet("/teams", data);
         assert(db.internalDB.getData("/teams")["test1"] === data.test1);
+    });
+
+    it("should push an object in a dictionary", () => {
+        let db = new TypedJsonDB<DbContent>("config.json");
+        db.dictionaryPush("/teams", "test1", "coucou");
+        assert(db.internalDB.getData("/teams/test1") === "coucou");
+    });
+
+    it("should push an object in a dictionary", () => {
+        let db = new TypedJsonDB<DbContent>("config.json");
+        db.dictionaryPush("/teams", "test2", "coucou");
+        assert(db.dictionaryGetAt("/teams", "test2") === "coucou");
+    });
+
+    it("should delete anything", () => {
+        let db = new TypedJsonDB<DbContent>("config.json");
+        db.delete("/restaurants");
+        assert(!db.exists("/restaurants"));
     });
 });
